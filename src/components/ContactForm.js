599b4242d83efcda9147f478b6361b316ed5b755
Carl Barrdahl <carlbarrdahl@gmail.com>
2019-04-28 11:25:49 +0200

    Handle ContactForm submit states

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "tachyons-components"
 
 const Form = styled("form")``
@@ -19,26 +19,104 @@ const Button = styled("button")`
   ph4 pv3
   grow no-underline dib
 `
-const ContactForm = () => (
-  <Form
-    onSubmit={e => {
-      e.preventDefault()
-      alert("not implemented")
-    }}
-  >
-    <Label>
-      Message
-      <Textarea required rows="4" placeholder="Enter your message..." />
-    </Label>
-    <Label>
-      Email
-      <Input
-        type="email"
-        required
-        placeholder="What is your email so I know where to reply?"
-      />
-    </Label>
-    <Button type="submit">Send email</Button>
-  </Form>
+const mapColors = ({ type }) => {
+  const colorMap = {
+    success: [ "dark-green", "bg-washed-green" ],
+    error: [ "dark-red", "bg-washed-red" ]
+  }
+  return (colorMap[type] || []).join(" ")
+}
+const Alert = styled("div")`
+ph4 pv4 br2 dark-green bg-washed-green
+${mapColors}
+`
+Alert.Title = styled("h3")`f3 mt0 mb2`
+Alert.Description = styled("p")`f4 ma0`
+const SuccessMessage = () => (
+  <Alert type="success">
+    <Alert.Title>Thanks!</Alert.Title>
+    <Alert.Description>
+      I have recevied your message and will be in touch shortly.
+    </Alert.Description>
+  </Alert>
 )
+const ErrorMessage = () => (
+  <Alert type="error">
+    <Alert.Title>Oops!</Alert.Title>
+    <Alert.Description>
+      Something went wrong with sending your message.
+    </Alert.Description>
+  </Alert>
+)
+const Loader = () => (
+  <div className="tc">
+    <pre>...</pre>
+  </div>
+)
+const states = {
+  LOADING: "LOADING",
+  SUCCESS: "SUCCESS",
+  FAILURE: "FAILURE"
+}
+const statusMap = {
+  [states.LOADING]: <Loader />,
+  [states.SUCCESS]: <SuccessMessage />,
+  [states.FAILURE]: <ErrorMessage />
+}
+const ContactForm = () => {
+  const [ status, setStatus ] = useState(null)
+  return status ? (
+    statusMap[status]
+  ) : (
+    <Form
+      name="contact"
+      method="POST"
+      action="/"
+      data-netlify
+      netlify-honeypot="bot-field"
+      onSubmit={e => {
+        e.preventDefault()
+        const { method, email, message } = e.target
+        const form = { email: email.value, message: message.value }
+        setStatus(states.LOADING)
+        fetch("/", {
+          method,
+          body: encode({ "form-name": "contact", ...form }),
+          headers: { "Content-Type": "application/x-www-form-urlencoded" }
+        })
+          .then(res => setStatus(res.ok ? states.SUCCESS : states.FAILURE))
+          .catch(() => setStatus(states.FAILURE))
+      }}
+    >
+      <input name="bot-field" style={{ visibility: "hidden" }} />
+      <input type="hidden" name="form-name" value="contact" />
+      <Label>
+        Message
+        <Textarea
+          name="message"
+          required
+          rows="4"
+          placeholder="Enter your message..."
+        />
+      </Label>
+      <Label>
+        Email
+        <Input
+          name="email"
+          type="email"
+          required
+          placeholder="What is your email so I know where to reply?"
+        />
+      </Label>
+      <Button type="submit">Send email</Button>
+    </Form>
+  )
+}
+
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
+
 export default ContactForm
