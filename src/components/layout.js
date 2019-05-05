be56806bbd3adc24767bad7cc8192b38554db7dc
Carl Barrdahl <carlbarrdahl@gmail.com>
2019-05-05 14:52:40 +0200

    Update layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,22 +6,26 @@ import Header from "./header"
 import "./layout.css"
 
 const Layout = styled("main")`w-100 bg-white-0125 dark-gray`
-const Footer = styled("footer")`tc pa4`
+const Footer = styled("footer")`tc`
 
-const mapColor = ({ bg }) => `bg-${bg}`
-export const Section = styled("section")`ph3 pv3 ph5-ns pb5 
+const mapColor = ({ bg, color }) => `bg-${bg} ${color}`
+export const Section = styled("section")`ph3 pv5 ph5-ns pb5 
 ${mapColor}`
 // bb b--black-10
-Section.Title = styled("h1")`f5 mt4 ttu tracked`
+Section.Title = styled("h1")`f5 ttu tracked`
 
 // export const Section = styled("section")`ph3 pv3 ph5-ns pb5`
 export const Container = styled("div")`mw8 center`
 
 export default ({ children }) => (
   <Layout>
-    <Header />
+    {/* <Header /> */}
     {children}
-    <Footer>© {new Date().getFullYear()} Carl B</Footer>
+    <Section bg="navy" color="near-white">
+      <Container>
+        <Footer>© {new Date().getFullYear()} Carl B</Footer>
+      </Container>
+    </Section>
   </Layout>
 )
 
