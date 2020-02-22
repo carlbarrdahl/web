368bfa504aad01e29883eae6c907d0a2cc9399a7
Carl Barrdahl <carlbarrdahl@gmail.com>
2020-02-22 11:40:24 +0100

    Add work

--- src/layout.js
+++ src/layout.js
@@ -1,17 +1,17 @@
 /** @jsx jsx */
-import { jsx, Styled } from "theme-ui"
-import { Link } from "gatsby"
+import { jsx, Styled } from "theme-ui";
+import { Link } from "gatsby";
 
 const NavLink = props => (
   <Styled.a as={Link} sx={{ variant: "styles.navlink", mr: 3 }} {...props} />
-)
+);
 export default props => {
   return (
     <Styled.root sx={{}}>
       <header sx={{ variant: "styles.header" }} role="banner">
         <nav role="navigation">
           <NavLink to="/">Home</NavLink>
-          <NavLink to="/">Work</NavLink>
+          <NavLink to="/work">Work</NavLink>
           <NavLink to="/about">About</NavLink>
         </nav>
       </header>
@@ -20,5 +20,5 @@ export default props => {
         <small>&#9400; {new Date().getFullYear()} Carl Barrdahl</small>
       </footer>
     </Styled.root>
-  )
-}
+  );
+};
